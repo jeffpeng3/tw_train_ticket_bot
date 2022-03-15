51194cf22c51cc22fddbcaa0b64b4a8fba16a95c
karl2015y <karl2015y@gmail.com>
2022-03-16 01:17:34 +0800

    ok

--- server.py
+++ server.py
@@ -320,7 +320,7 @@ def send_js(path):
 # app.run(host="0.0.0.0", port=8000)
 
 if __name__ == "__main__":
-    os.mkdir("audio")
-    os.mkdir("log")
+    os.makedirs("./audio")
+    os.makedirs("./log")
     port = int(os.environ.get("PORT", 8000))
     app.run(host='0.0.0.0', port=port)
