3b43ab09b5f352c124bd6852a48587315695aa14
jeff <jeffpeng2012@gmail.com>
2025-05-08 03:59:46 +0800

    vcb

--- booking_logic.py
+++ booking_logic.py
@@ -2,15 +2,17 @@ import aiohttp
 from asyncio import sleep
 from bs4 import BeautifulSoup, Tag
 import captcha_handler
-from dataclasses import dataclass, field
-from typing import Any, Literal
+from dataclasses import dataclass
+from typing import Any
 from datetime import datetime, timedelta
 from time import time
 from enum import Enum
 
+
 class Mode(Enum):
     ticket = "BY_TRAIN_NO"
     time = "BY_TIME"
+
 
 @dataclass
 class Ticket:
@@ -38,6 +40,33 @@ class Ticket:
         else:
             raise ValueError("Invalid mode")
 
+    def to_payload(
+        self, f_token: str, quick_tip_token: str, captcha_code: str
+    ) -> list[tuple[str, str]]:
+        payload: list[tuple[str, str]] = [
+            ("pid", self.pid),
+            ("date", self.date),
+            ("start", self.start),
+            ("end", self.end),
+            ("mode", self.mode.value),
+        ]
+        if self.mode == Mode.time:
+            payload.extend(
+                [
+                    ("start_time", self.start_time),
+                    ("end_time", self.end_time),
+                    ("train_type", ",".join(map(str, self.train_type))),
+                ]
+            )
+        else:
+            payload.extend(
+                [
+                    ("train", ",".join(self.train)),
+                ]
+            )
+        return payload
+
+
 async def _get_initial_page_and_tokens(
     session: aiohttp.ClientSession,
 ) -> tuple[str, str]:
@@ -64,9 +93,11 @@ async def _resolve_captcha(
         "https://www.railway.gov.tw/tra-tip-web/tip/player/nonPicture?pageRandom=123"
     ) as non_pic_response:
         non_pic_response.raise_for_status()
-        non_pic_content: str = await non_pic_response.text()
+        _: str = await non_pic_response.text()
 
-    url: str = "https://www.railway.gov.tw/tra-tip-web/tip/player/audio?pageRandom=1079330974"
+    url: str = (
+        "https://www.railway.gov.tw/tra-tip-web/tip/player/audio?pageRandom=1079330974"
+    )
     verifyCode: str = await captcha_resolver.resolve_audio_captcha(session, url)
     return verifyCode
 
@@ -81,29 +112,22 @@ async def _prepare_booking_data(
         "startStation": config.start,
         "endStation": config.end,
         "tripType": "ONEWAY",
-        "orderType": "BY_TRAIN_NO",
         "normalQty": 1,
         "wheelChairQty": 0,
         "parentChildQty": 0,
         "ticketOrderParamList[0].tripNo": "TRIP1",
-        "ticketOrderParamList[0].startOrEndTime": "true"
-
-        "ticketOrderParamList[0].rideDate": config.date,
-        "ticketOrderParamList[0].trainNoList[0]": "",
-        "ticketOrderParamList[0].trainNoList[1]": "",
-        "ticketOrderParamList[0].trainNoList[2]": "",
-        "ticketOrderParamList[0].seatPref": "NONE",
         "ticketOrderParamList[0].chgSeat": "true",
         "_ticketOrderParamList[0].chgSeat": "on",
-        "ticketOrderParamList[0].trainTypeList": 11,
-        "ticketOrderParamList[0].trainTypeList": 2,
-
-        "g-recaptcha-response": "",
-        "hiddenRecaptcha": "",
+        "ticketOrderParamList[0].seatPref": "NONE",
         "verifyType": "voice",
         "verifyCode": captcha_code,
+        "orderType": "BY_TRAIN_NO",
+        "g-recaptcha-response": "",
+        "hiddenRecaptcha": "",
         "quickTipToken": quick_tip_token,
+        "ticketOrderParamList[0].rideDate": config.date,
     }
+    # "ticketOrderParamList[0].startOrEndTime": "true",
     if config.mode == Mode.ticket:
         for i, train_no in enumerate(config.train):
             train_data[f"ticketOrderParamList[0].trainNoList[{i}]"] = train_no
@@ -124,7 +148,8 @@ async def _submit_booking_request(
     error_div: Tag | None = soup.find("div", {"id": "errorDiv"})
     return not error_div
 
-async def process_booking(config: BookingConfig) -> None:
+
+async def process_booking(config: Ticket) -> None:
     user_agent: str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36"
     headers: dict[str, str] = {"User-Agent": user_agent}
     captcha_resolver_instance = captcha_handler.CaptchaResolver()
@@ -134,7 +159,9 @@ async def process_booking(config: BookingConfig) -> None:
             t1: float = time()
             csrf, quick_tip = await _get_initial_page_and_tokens(session)
             captcha = await _resolve_captcha(session, captcha_resolver_instance)
-            booking_data: dict[str, Any] = await _prepare_booking_data(config, csrf, quick_tip, captcha)
+            booking_data: dict[str, Any] = await _prepare_booking_data(
+                config, csrf, quick_tip, captcha
+            )
             if await _submit_booking_request(session, booking_data):
                 print("訂票成功！")
                 break
--- server.py
+++ server.py
@@ -1,24 +1,25 @@
 import asyncio
-import aiohttp
 from booking_logic import (
-    BookingConfig,
+    Ticket,
+    Mode,
     process_booking
 )
 
 async def main():
-    example_config = BookingConfig(
-        person_id="J123136682",
-        from_station="1000-臺北",
-        to_station="3300-臺中",
-        getin_date="2025/05/09",
-        order_type="BY_TIME",
-        train_no=["111", "123"],
-        ticket_num=1,
-        can_change_seat=True,
+    example_config = Ticket(
+        pid="J123136682",
+        start="1000-臺北",
+        end="3300-臺中",
+        date="2025/05/09",
+        mode=Mode.time,
+        train=[],
+        train_type=[True, True, True],
+        start_time="10:00",
+        end_time="12:00",
     )
-    print(f"準備開始處理 {example_config.person_id} 的訂票請求...")
+    print(f"準備開始處理 {example_config.pid} 的訂票請求...")
     await process_booking(example_config)
-    print(f"{example_config.person_id} 的訂票請求處理完畢。請查看上方日誌以了解結果。")
+    print(f"{example_config.pid} 的訂票請求處理完畢。請查看上方日誌以了解結果。")
 
 
 if __name__ == "__main__":
